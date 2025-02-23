0b23d428c9af26a2479491c18fef98ce03a56488
Niakrist <ak_88dron@inbox.ru>
2025-02-23 09:54:18 +0300

    fix: fix mini bug

--- app/controller/bodyController.js
+++ app/controller/bodyController.js
@@ -3,7 +3,6 @@ class BodyController {
   async createBody(req, res) {
     try {
       const { name, title, description, h1, url } = req.body;
-
       const isHaveBody = await Body.findOne({ where: { name } });
       if (isHaveBody) {
         return res
@@ -16,7 +15,6 @@ class BodyController {
       return res.status(500).json(error);
     }
   }
-
   async getAllBody(req, res) {
     try {
       const bodys = await Body.findAll();
@@ -25,22 +23,18 @@ class BodyController {
       return res.status(400).json(error);
     }
   }
-
   async editBody(req, res) {
     try {
       const { id, name, title, description, h1, url } = req.body;
       if (!id) {
         res.status(400).json({ message: `Категории id: ${id} не найдено` });
       }
-
       const body = await Body.findOne({ where: { id } });
-
       body.name = name ? name : body.name;
       body.title = title ? title : body.title;
       body.description = description ? description : body.description;
       body.h1 = h1 ? h1 : body.h1;
       body.url = url ? url : body.url;
-
       await body.save();
       return res.json(body);
     } catch (error) {
